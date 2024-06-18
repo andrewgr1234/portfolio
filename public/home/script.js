98cb7d384a80d8c24336e2e65e05513bf67a57c8
andrewgr1234 <85031521+andrewgr1234@users.noreply.github.com>
2024-06-18 11:19:13 +0300

    touches
    Signed-off-by: andrewgr1234 <85031521+andrewgr1234@users.noreply.github.com>

--- public/home/script.js
+++ public/home/script.js
@@ -1,4 +1,23 @@
-setTimeout(() => {
+/*setTimeout(() => {
   document.getElementById("loader").style.display = "none";
   document.getElementsByTagName("main")[0].style.display = "block";
-}, 1500);
+}, 1500);*/
+
+function applyAnimationToAllChildrenOfMain(animation, duration) {
+  const mainElement = document.querySelector("main");
+  if (mainElement) {
+    const children = mainElement.querySelectorAll("*");
+    children.forEach((child) => {
+      child.style.animation = animation;
+      child.style.animationDuration = duration;
+    });
+  }
+}
+applyAnimationToAllChildrenOfMain("fadeInDown", "2s");
+
+let year = new Date().getFullYear();
+let age = year - 2009;
+
+document.getElementById(
+  "introtxt"
+).innerHTML = `Welcome to my journey in the Hi-Tech world. <br/> im currently ${age} years old and an Intern at Loop.`;
